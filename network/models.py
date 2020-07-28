c0cc9d28fc7f9af8e7264e3c513ae3275508fdaf
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-07-28 11:46:25 -0400

    add post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    username = models.ForeignKey(
+        "User", default=1, on_delete=models.CASCADE, related_name="all_posts")
+    sender = models.ForeignKey(
+        "User", default=1, on_delete=models.PROTECT, related_name="post_sent")
+    content = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "sender": self.sender.post,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%b %-d %Y, %-I:%M %p"),
+            "likes": self.likes
+        }

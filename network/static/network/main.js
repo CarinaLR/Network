936381c968b8f3238004017786075d51c5332db6
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-07-29 15:28:00 -0400

    add script

--- network/static/network/main.js
+++ network/static/network/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#all_post").addEventListener("click", () => {
+  document.querySelector("#post-button").addEventListener("click", () => {
     const box_post = document.createElement("div");
     box_post.innerHTML = "I'm in All Post view";
   });

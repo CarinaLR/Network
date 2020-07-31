1e546f5c12dba503399dc4f2ca382291513d7c7b
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-07-31 08:13:43 -0400

    add event listener

--- network/static/network/main.js
+++ network/static/network/main.js
@@ -1,5 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#post-button").addEventListener("click", () => {
-    console.log("I'm here!");
-  });
+  document
+    .querySelector("#show_posts")
+    .addEventListener("click", () => load_posts());
 });
+
+function load_posts() {
+  // Show more post and hide sample
+  document.querySelector("#post-view").style.display = "block";
+  document.querySelector("#compose-post").style.display = "none";
+
+  // if (mailbox === "inbox") {
+  //   // Load inbox emails
+  //   fetch("/emails/inbox")
+  //     .then((response) => response.json())
+  //     .then((emails) => {
+  //       emails.forEach(add_emailsInbox);
+  //     });
+  // }
+  // if (mailbox === "sent") {
+  //   // Load sent emails
+  //   fetch("/emails/sent")
+  //     .then((response) => response.json())
+  //     .then((emails) => {
+  //       emails.forEach(add_emailsSent);
+  //     });
+  // }
+  // if (mailbox === "archive") {
+  //   // Load archive emails
+  //   fetch("/emails/archive")
+  //     .then((response) => response.json())
+  //     .then((emails) => {
+  //       emails.forEach(add_emailsArchive);
+  //     });
+  // }
+}

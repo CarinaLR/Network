beff5b69d7483fb8370d991ce7a4d030c49317e2
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-08-11 08:41:28 -0400

    editing post

--- network/static/network/main.js
+++ network/static/network/main.js
@@ -33,75 +33,67 @@ function unfollow_button() {
 function edit_post(post_id) {
   console.log("post_id -> ", post_id);
   post_id = post_id;
-  // const query_selector = document.querySelector(".compose-post").click(() => {
-  //   let id_post = $(this).data("id");
-  //   console.log("id_post -> ", id_post);
-  // });
-  // console.log("query_selector - ", query_selector);
-
+  // Block to get info from post
   if (post_id) {
-    // document.querySelector("#titlePage").style.display = "none";
-    // document.querySelector("#inputText").style.display = "none";
-    // document.querySelector("#timeInfo").style.display = "none";
     // Get value from data-id
     get_class = document.querySelector(".compose-post");
     get_id = get_class.getAttribute("data-id");
-    console.log("get_id - ", get_id);
+
     // Get request by id.
     fetch(`/post/${post_id}`)
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          console.log("response", response);
           post_view(response);
         } else {
           console.log("not found");
         }
       });
-
-    console.log("I'm here");
   }
-
-  // const blockDiv = document.createElement("div");
-  // blockDiv.className = "compose-edit";
-
-  // const newDiv = document.createElement("div");
-  // newDiv.className = "newDiv";
-
-  // const titlePage = document.createElement("h3");
-  // titlePage.id = "titlePage";
-  // titlePage.innerHTML = "Edit Post";
-
-  // const formPost = document.createElement("form");
-  // formPost.className = "form-group";
-  // formPost.method = "POST";
-
-  // const textarea = document.createElement("textarea");
-  // textarea.id = "content";
-  // textarea.className = "form-group";
-  // textarea.name = "content";
-
-  // const break_p = document.createElement("br");
-
-  // const input_edit = document.createElement("input");
-  // input_edit.type = "submit";
-  // input_edit.id = "post-button";
-  // input_edit.className = "btn btn-primary";
-  // input_edit.value = "Save";
-  // input_edit.name = "post";
-
-  // formPost.appendChild(textarea);
-  // formPost.appendChild(break_p);
-  // formPost.appendChild(input_edit);
-
-  // newDiv.appendChild(titlePage);
-  // newDiv.appendChild(formPost);
-
-  // // Show textarea post and hide post
-  // document.querySelector(".compose-post").style.display = "none";
-  // document.querySelector(".compose-edit").style.display = "block";
 }
 
-function post_view(post_id) {
-  console.log("IDK ->", post_id);
+function post_view(response) {
+  console.log("IDK ->", response);
+  // Get post content to populate textarea
+  let content_post = response.post_content;
+  console.log("content ->", content_post);
+
+  // Get div where the edit post view will be display
+  const blockDiv = document.querySelector(".compose-edit");
+
+  // Create elements for edit post
+  const newDiv = document.createElement("div");
+  newDiv.className = "newDiv";
+
+  const titlePage = document.createElement("h3");
+  titlePage.id = "titlePage";
+  titlePage.innerHTML = "Edit Post";
+
+  const formPost = document.createElement("form");
+  formPost.className = "form-group";
+  formPost.method = "POST";
+
+  const textarea = document.createElement("textarea");
+  textarea.id = "content";
+  textarea.className = "form-group";
+  textarea.name = "content";
+  textarea.innerHTML = content_post;
+
+  const break_p = document.createElement("br");
+
+  const input_edit = document.createElement("input");
+  input_edit.type = "submit";
+  input_edit.id = "post-button";
+  input_edit.className = "btn btn-primary";
+  input_edit.value = "Save";
+  input_edit.name = "post";
+
+  // Append all children to the main div to display block
+  formPost.appendChild(textarea);
+  formPost.appendChild(break_p);
+  formPost.appendChild(input_edit);
+
+  newDiv.appendChild(titlePage);
+  newDiv.appendChild(formPost);
+  blockDiv.appendChild(newDiv);
 }
